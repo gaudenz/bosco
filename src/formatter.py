27896eecc23681a7f0d15dcfbaefa64940de0502
Gaudenz Steinlin <gaudenz@soziologie.ch>
2008-04-17 08:35:05 +0200

    add additional information to formatted rankings
    Two new arguments allow the setting of header information
    and ranking descriptions as dicts.

--- src/formatter.py
+++ src/formatter.py
@@ -19,8 +19,9 @@ formatter.py - Classes to to format rankings
 
 from mako.template import Template
 from mako.lookup import TemplateLookup
+from datetime import datetime
 
-import conf
+from ranking import ValidationStrategy
 
 class AbstractRankingFormatter(object):
     """Formats a ranking. str(rankingRormatter) returns the formatted ranking."""
@@ -44,14 +45,34 @@ class AbstractRankingFormatter(object):
 class MakoRankingFormatter(AbstractRankingFormatter):
     """Uses the Mako Templating Engine to format a ranking as HTML."""
 
-    _lookup = TemplateLookup(directories=[conf.template_dir])
-
-    def __init__(self, ranking, template_file):
+    _validation_codes = {ValidationStrategy.NOT_COMPLETED    : 'not yet finished',
+                         ValidationStrategy.MISSING_CONTROLS : 'missing controls',
+                         ValidationStrategy.DID_NOT_FINISH   : 'did not finish',
+                         ValidationStrategy.DISQUALIFIED     : 'disqualified',
+                         ValidationStrategy.DID_NOT_START    : 'did not (yet) start'}
+    
+    def __init__(self, ranking, header, info, template_file, template_dir):
+        """
+        @type ranking:        object of class Ranking
+        @param template_file: File name for the template
+        @param header:        gerneral information for the ranking header
+        @type header:         dict
+        @param info:          inforamtions about the ranking (course code, ...)
+        @type info:           dict
+        """
         super(type(self), self).__init__(ranking)
-        self._template = MakoRankingFormatter._lookup.get_template(template_file)
+        lookup = TemplateLookup(directories=[template_dir])
+        self._template = lookup.get_template(template_file)
+        self._header = header
+        self._info = info
 
     def __str__(self):
-        return self._template.render(title = 'hier steht der titel', ranking = self._ranking)
+
+        return self._template.render_unicode(header = self._header,
+                                             info = self._info,
+                                             validation_codes = self._validation_codes,
+                                             now = datetime.now().strftime('%c'),
+                                             ranking = self._ranking)
 
         
     
