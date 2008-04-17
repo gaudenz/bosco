22a257c642815939ffe1c2d0f5203dc3c88c7201
Gaudenz Steinlin <gaudenz@soziologie.ch>
2008-04-17 08:39:20 +0200

    observe changes in objects instead of events
    Users can register objects with the EventObserver
    and the observer notifies them if an event happens
    which affects the object.
    
    The downside is that EventObserver no longer uses it's
    own database connection and therefore rollbacks the store
    every now and then.

--- src/observer.py
+++ src/observer.py
@@ -24,61 +24,98 @@ from run import Punch
 import conf
 
 class EventObserver(object):
-    """Observes an Event for new data (e.g. new punches)"""
+    """Observes an Event for new data (e.g. new punches).
+    @warn: This will rollback the store every <intervall> seconds!
+    """
 
-    def __init__(self, interval = 5):
+    def _punch_notify(self, punch):
+        
+        # send notifications for run
+        if punch.run in self._registry:
+            self._notify(punch.run)
+            
+        # send notifications for runner
+        if punch.run.sicard.runner in self._registry:
+            self._notify(punch.run.sicard.runner)
+
+        # send notifications for team
+        if punch.run.sicard.runner.team in self._registry:
+            self._notify(punch.run.sicard.runner.team)
+            
+    _tables = [(Punch, _punch_notify),
+               ]
+    
+    def __init__(self, store, interval = 5):
 
         self._interval = interval
         
         # allocate own store and db connection to make sure this is independant of
         # running transactions
-        self._store = Store(create_database(conf.db_uri))
+        self._store = store
         
-        self._registry = {'punch': []}
+        self._registry = {}
 
-        self._last = {'punch': None}
-        self._running = True
-        self.observe() # this also starts the timer
+        self._last = {}
+        for t in EventObserver._tables:
+            self._last[t[0]] = self._get_max(t[0])
+            
+        self._running = False
         
-
     def _start_timer(self):
-        if not self._running == False:
+        if self._running == True:
             t = Timer(self._interval, self.observe)
             t.start()
-
+            
     def stop(self):
         self._running = False
         
-    def register(self, obj, event):
+    def register(self, obj, observable):
         """Register an object to be notified of changes of type event.
 
-        @param obj:   The object to receive to notification. obj must have a
-                      update(self, event) method.
-        @param event: Event type to observe. Currently only 'punch'
+        @param obj:        The object to receive to notification. obj must have a
+                           update(self, event) method.
+        @param observable: object that should be observed for changes. Currently
+                           run, runner and team are supported
         """
-        self._registry[event].append(obj)
+        if not observable in self._registry:
+            self._registry[observable] = []
+        self._registry[observable].append(obj)
+        if self._running == False:
+            self._running = True
+            self._start_timer()
 
-    def unregister(self, obj, event):
-        """Unregister an object from this observer for this event."""
-        self._registry[event].remove(obj)
+    def unregister(self, obj, observable):
+        """Unregister an object from this observer for this observable."""
+        self._registry[observable].remove(obj)
+        if len(self._registry[observable]) == 0:
+            del(self._registry[observable])
+        if len(self._registry) == 0:
+            self._running = False
         
     def observe(self):
         """Does the actual observation."""
 
         # rollback store to end transaction
         self._store.rollback()
-        
-        last_punch = self._store.execute(Select(Max(Punch.id))).get_one()
-        if self._last['punch'] != last_punch:
-            self._last['punch'] = last_punch
-            self._notify('punch')
 
+        for t in EventObserver._tables:
+            last = self._get_max(t[0])
+            if self._last[t[0]] != last:
+                for i in range(self._last[t[0]]+1, last+1):
+                    obj = self._store.get(t[0], i)
+                    if not obj is None:
+                        t[1](self, obj)
+            self._last[t[0]] = last
+            
         # start new timer
         self._start_timer()
 
-    def _notify(self, event):
+    def _get_max(self, t):
+        return self._store.execute(Select(Max(t.id))).get_one()[0] or 0
+    
+    def _notify(self, observable):
         """Notifies objects of an event."""
-        for obj in self._registry[event]:
-            obj.update(event)
+        for obj in self._registry[observable]:
+            obj.update(observable)
 
     
