b64dc4b76f404cd11b259537906f844d66557aa3
Gaudenz Steinlin <gaudenz@soziologie.ch>
2008-05-10 11:42:26 +0200

    add example configurations

---
+++ examples/conf24h.py
@@ -0,0 +1,113 @@
+#/usr/bin/env python
+#    config.py - Local Configuration
+#
+#    Copyright (C) 2008  Gaudenz Steinlin <gaudenz@soziologie.ch>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from storm.locals import *
+from datetime import datetime, timedelta
+
+from ranking import (ControlPunchtimeScoreing, OpenRuns,
+                     Cache)
+from event import Relay24hEvent
+from course import Course, Control
+from runner import Category
+from observer import TriggerEventObserver
+                    
+# Database URI and store
+db_uri = 'postgres://24h:forst@server/24h'
+#db_uri = 'postgres:24h'
+store = Store(create_database(db_uri))
+
+# Directory with templates
+template_dir = 'templates'
+
+# create cache (but don't connect to an observer)
+cache = Cache()
+
+# create an Event Observer
+observer = TriggerEventObserver(store)
+
+# EventRanking object
+starttime = datetime(2008,4,30,21,55)
+_cat24h = store.find(Category, Category.name == u'24h').one()
+_cat12h = store.find(Category, Category.name == u'12h').one()
+_last_ctrls = [store.find(Control, Control.code == u'31').one(),
+               store.find(Control, Control.code == u'32').one()]
+
+event = Relay24hEvent(starttime_24h = starttime,
+                      starttime_12h = datetime(2008,4,30,22,50),
+                      speed = 1.5,
+                      extra_rankings = [('24h_lkm',
+                                         {'obj':_cat24h,
+                                          'scoreing_args':{'method':'lkm'}
+                                          }
+                                         ),
+                                        ('12h_lkm',
+                                         {'obj':_cat12h,
+                                          'scoreing_args':{'method':'lkm'}
+                                          }
+                                         ),
+                                        ('24h_start',
+                                         {'obj':_cat24h,
+                                          'scoreing_args':{'blocks':'start'},
+                                          'validation_args':{'blocks':'start'}
+                                          }
+                                         ),
+                                        ('24h_night',
+                                         {'obj':_cat24h,
+                                          'scoreing_args':{'blocks':'night'},
+                                          'validation_args':{'blocks':'night'}
+                                          }
+                                         ),
+                                        ('24h_day',
+                                         {'obj':_cat24h,
+                                          'scoreing_args':{'blocks':'day'},
+                                          'validation_args':{'blocks':'day'}
+                                          }
+                                         ),
+                                        ('12h_start',
+                                         {'obj':_cat12h,
+                                          'scoreing_args':{'blocks':'start'},
+                                          'validation_args':{'blocks':'start'}
+                                          }
+                                         ),
+                                        ('12h_day',
+                                         {'obj':_cat12h,
+                                          'scoreing_args':{'blocks':'day'},
+                                          'validation_args':{'blocks':'day'}
+                                          }
+                                         ),
+                                        ('warning',
+                                         {'obj':OpenRuns(store),
+                                          'scoreing_args': {'control_list': _last_ctrls,
+                                                           },
+                                          }
+                                         ),
+                                        ],
+                      header = {'event'  : '24 Stunden Orientierungslauf Schweiz 2008',
+                                'map'    : 'Forst',
+                                'place'  : 'Heitere, Neuenegg',
+                                'date'   : '11./12. May 2008',
+                                'organiser' : 'UBOL / OLG Bern'},
+                      duration_24h = timedelta(hours=2, minutes = 6),
+                      duration_12h = timedelta(hours=1, minutes = 3),
+                      cache = cache, store = store)
+                                     
+                                     
+#rankings['warning'] = MakoRankingFormatter(ranking, header,
+#                                           {'title': 'Advance Warning'},
+#                                           'warning.html', template_dir)
+                                   
---
+++ examples/confubol3.py
@@ -0,0 +1,61 @@
+#/usr/bin/env python
+# -*- coding: utf-8 -*-
+#    config.py - Local Configuration
+#
+#    Copyright (C) 2008  Gaudenz Steinlin <gaudenz@soziologie.ch>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from storm.locals import *
+from datetime import datetime, timedelta
+
+from ranking import (ControlPunchtimeScoreing, OpenRuns,
+                     Cache)
+from formatter import MakoRankingFormatter
+from event import RelayEvent
+from course import Course, Control
+from runner import Category
+                    
+# Database URI and store
+db_uri = 'postgres:ubol3'
+store = Store(create_database(db_uri))
+
+# Directory with templates
+template_dir = 'templates'
+
+# create cache (but don't connect to an observer)
+cache = Cache()
+
+event = RelayEvent(legs = [(u'0', datetime(2008,5,1,11,50)),
+                           (u'1', datetime(2008,5,1,11,55)),
+                           (u'2', datetime(2008,5,1,13,10)),
+                           (u'3', datetime(2008,5,1,14,10))],
+                   header = {'event'     : u'Jura 3er-Staffel 2008',
+                             'map'       : u'Tete Plumée',
+                             'place'     : u'Les Trois Chênes',
+                             'date'      : u'1. May 2008',
+                             'organiser' : u'UBOL'},
+                   extra_rankings = [('warning',
+                                      {'obj':OpenRuns(store),
+                                       'scoreing_args': {'control_list': [store.find(Control, Control.code == u'155').one()],
+                                                         },
+                                       }
+                                      ),
+                                     ],
+                   html_template = '24h.html',
+                   store = store,
+                   cache = cache
+                   )
+
+                    
