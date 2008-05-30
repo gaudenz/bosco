3bf057b6da0162cd550afebeb1f42b73492226b1
Gaudenz Steinlin <gaudenz@soziologie.ch>
2008-05-30 12:17:17 +0200

    remove unneeded import
    Observer now uses a store for it's operation, thus
    import conf is no longer needed.

--- src/observer.py
+++ src/observer.py
@@ -23,7 +23,6 @@ from datetime import datetime
 
 from run import Punch, Run
 from runner import Team
-import conf
 
 class EventObserver(object):
     """Observes an Event for new data (e.g. new punches).
