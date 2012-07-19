c7190f06e128ca0c2de062e15f81c6dfa5c0b82c
Gaudenz Steinlin <gaudenz@soziologie.ch>
2012-07-19 22:05:17 +0200

    More SI "documentation" and some fixes
    No functional changes. Thanks to Simon Harston <simon@harston.de>
    for all of this information.

--- bosco/sireader.py
+++ bosco/sireader.py
@@ -34,14 +34,32 @@ class SIReader(object):
     # Protocol characters
     STX           = '\x02'
     ETX           = '\x03'
-    ACK           = '\x06'
+    ACK           = '\x06' # when sent to BSx3..6, causes beep until SI-card taken out
     NAK           = '\x15'
     DLE           = '\x10'
-    
+    WAKEUP        = '\xFF'
 
-    # Basic protocol commands
-    BC_SET_MS     = '\x70'
-    
+    # Basic protocol commands, currently unused
+    BC_SET_CARDNO = '\x30'
+    BC_GET_SI5    = '\x31' # read out SI-card 5 data
+    BC_SI5_WRITE  = '\x43' # 02 43 (block: 0x30 to 0x37) (16 bytes) 03
+    BC_SI5_DET    = '\x46' # SI-card 5 inserted (46 49) or removed (46 4F)
+    BC_TRANS_REC  = '\x53' # autosend timestamp (online control)
+    BC_TRANS_TIME = '\x54' # autosend timestamp (lightbeam trigger)
+    BC_GET_SI6    = '\x61' # read out SI-card 6 data
+    BC_SI6_DET    = '\x66' # SI-card 6 inserted
+    BC_SET_MS     = '\x70' # \x4D="M"aster, \x53="S"lave
+    BC_GET_MS     = '\x71'
+    BC_SET_SYS_VAL= '\x72'
+    BC_GET_SYS_VAL= '\x73'
+    BC_GET_BDATA  = '\x74' # Note: response carries '\xC4'!
+    BC_ERASE_BDATA= '\x75'
+    BC_SET_TIME   = '\x76'
+    BC_GET_TIME   = '\x77'
+    BC_OFF        = '\x78'
+    BC_GET_BDATA2 = '\x7A' # Note: response carries '\xCA'!
+    BC_SET_BAUD   = '\x7E' # 0=4800 baud, 1=38400 baud
+
     # Extended protocol commands
     C_GET_BACKUP  = '\x81'
     C_SET_SYS_VAL = '\x82'
@@ -58,18 +76,63 @@ class SIReader(object):
     C_ERASE_BDATA = '\xF5'
     C_SET_TIME    = '\xF6'
     C_GET_TIME    = '\xF7'
+    C_OFF         = '\xF8'
     C_BEEP        = '\xF9'
     C_SET_BAUD    = '\xFE'
 
     # Protocol Parameters
-    P_MS_DIRECT   = '\x4D'
-    P_MS_INDIRECT = '\x53'
+    P_MS_DIRECT   = '\x4D' # "M"
+    P_MS_INDIRECT = '\x53' # "S"
     P_SI6_CB      = '\x08'
 
     # offsets in system data
-    O_MODE        = '\x71'
-    O_STATION_CODE= '\x72'
-    O_PROTO       = '\x74'
+    # Thanks to Simon Harston <simon@harston.de> for most of this information
+    # currently only O_MODE, O_STATION_CODE and O_PROTO are used
+    O_OLD_SERIAL  = '\x00' # 2 bytes - only up to BSx6, numbers < 65.536
+    O_OLD_CPU_ID  = '\x02' # 2 bytes - only up to BSx6, numbers < 65.536
+    O_SERIAL_NO   = '\x00' # 4 bytes - only after BSx7, numbers > 70.000
+                           #   (if byte 0x00 > 0, better use OLD offsets)
+    O_FIRMWARE    = '\x05' # 3 bytes
+    O_BUILD_DATE  = '\x08' # 3 bytes - YYMMDD
+    O_MODEL_ID    = '\x0B' # 2 bytes:
+                           #   8003: BSF3 (serial numbers > 1.000)
+                           #   8004: BSF4 (serial numbers > 10.000)
+                           #   8084: BSM4-RS232
+                           #   8086: BSM6-RS232 / BSM6-USB
+                           #   8146: BSF6 (serial numbers > 30.000)
+                           #   8115: BSF5 (serial numbers > 50.000)
+                           #   8117 / 8118: BSF7 / BSF8 (serial no. 70.000...70.521, 72.002...72.009)
+                           #   8187 / 8188: BS7-SI-Master / BS8-SI-Master
+                           #   8197: BSF7 (serial numbers > 71.000, apart from 72.002...72.009)
+                           #   8198: BSF8 (serial numbers > 80.000)
+                           #   9197 / 9198: BSM7-RS232, BSM7-USB / BSM8-USB, BSM8-SRR
+                           #   9199: unknown
+                           #   9597: BS7-S
+                           #   B197 / B198: BS7-P / BS8-P
+                           #   B897: BS7-GSM
+    O_MEM_SIZE    = '\x0D' # 1 byte - in KB
+    O_BAT_DATE    = '\x15' # 3 bytes - YYMMDD
+    O_BAT_CAP     = '\x19' # 2 bytes - battery capacity in mAh (as multiples of 14.0625?!)
+    O_BACKUP_PTR  = '\x1C' # 4 bytes - at positions 1C,1D,21,22
+    O_SI6_CB      = '\x33' # 1 byte - bitfield defining which SI Card 6 blocks to read:
+                           #   \x00=\xC1=read block0,6,7; \x08=\xFF=read all 8 blocks
+    O_MEM_OVERFLOW= '\x3D' # 1 byte - memory overflow if != 0x00
+    O_PROGRAM     = '\x70' # 1 byte - station program: xx0xxxxxb competition, xx1xxxxxb training
+    O_MODE        = '\x71' # 1 byte - see SI station modes below
+    O_STATION_CODE= '\x72' # 1 byte
+    O_PROTO       = '\x74' # 1 byte - protocol configuration, bit mask value:
+                           #   xxxxxxx1b extended protocol
+                           #   xxxxxx1xb auto send out
+                           #   xxxxx1xxb handshake (only valid for card readout)
+                           #   xxx1xxxxb access with password only
+                           #   1xxxxxxxb read out SI-card after punch (only for punch modes;
+                           #             depends on bit 2: auto send out or handshake)
+    O_WAKEUP_DATE = '\x75' # 3 bytes - YYMMDD
+    O_WAKEUP_TIME = '\x78' # 3 bytes - 1 byte day (see below), 2 bytes seconds after midnight/midday
+    O_SLEEP_TIME  = '\x7B' # 3 bytes - 1 byte day (see below), 2 bytes seconds after midnight/midday
+                           #   xxxxxxx0b - seconds relative to midnight/midday: 0 = am, 1 = pm
+                           #   xxxx000xb - day of week: 000 = Sunday, 110 = Saturday
+                           #   xx00xxxxb - week counter 0..3, relative to programming date
 
     # SI station modes
     M_CONTROL     = '\x02'
@@ -77,7 +140,7 @@ class SIReader(object):
     M_FINISH      = '\x04'
     M_READOUT     = '\x05'
     M_CLEAR       = '\x07'
-    M_CHECK       = '\x0a'
+    M_CHECK       = '\x0A'
     SUPPORTED_MODES = (M_CONTROL, M_START, M_FINISH, M_READOUT, M_CLEAR, M_CHECK)
 
     # Weekday encoding (only for reference, currently unused)
@@ -87,7 +150,7 @@ class SIReader(object):
     D_WEDNESDAY   = 0b011
     D_THURSDAY    = 0b100
     D_FRIDAY      = 0b101
-    D_SATURDAY    = 0b111
+    D_SATURDAY    = 0b110
 
     # Backup memory record length
     REC_LEN       = 8
